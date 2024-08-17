4f9877120a0b6f9ae244b79213c32baa6a50ca95
gabrilov <trastolillo@tutanota.com>
2024-08-17 23:39:25 +0200

    configuraciones para lazyvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Reset del cursor para Alacritty
+vim.api.nvim_create_autocmd("ExitPre", {
+  group = vim.api.nvim_create_augroup("Exit", { clear = true }),
+  command = "set guicursor=a:ver90",
+  desc = "Set cursor back to beam when leaving Neovim.",
+})
+
+-- Remap para volver al modo normal
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
